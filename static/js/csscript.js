c31b08f44a4fb4ae50dcf6f8711026aace982e4c
codegyeon <wjdrlgus313@naver.com>
2022-02-24 06:16:52 +0900

    two

---
+++ static/js/csscript.js
@@ -0,0 +1,17 @@
+
+function changetitle(obj){
+    let title1 = document.getElementById('title1')
+    let title2 = document.getElementById('title2')
+    let title3 = document.getElementById('title3')
+
+    if (obj === title1) {
+        $("#maintitle").text(`${title1.innerText}`)
+    } else if (obj === title2) {
+        $("#maintitle").text(`${title2.innerText}`)
+    } else {
+        $("#maintitle").text(`${title3.innerText}`)
+    }
+}
+function chagepost(){
+    let post1 = document.getElementById('title1')
+}
